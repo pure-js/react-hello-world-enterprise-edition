e301cb6b8d5c4fd45e00ac0c4871993eeb90dc2a
Årthဝur <curious.dev@icloud.com>
2023-09-11 11:14:11 +0400

    feat: add hello world  component (#2)
    * feat: add hello world component
    
    * chore: re group dependencies
    
    * chore: show all options in tsconfig
    
    * chore: format on save settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { HelloWorld } from '@/components/hello-world';
 
 export default function Home() {
   return (
@@ -38,6 +39,8 @@ export default function Home() {
           priority
         />
       </div>
+
+      <HelloWorld />
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
