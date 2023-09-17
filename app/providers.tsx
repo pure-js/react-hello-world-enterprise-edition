8d34250ac72567e2a1edc45084f61fc850a8de5e
pure-js <3690368+pure-js@users.noreply.github.com>
2023-09-17 22:49:28 +0400

    build: install and setup TanStack Query

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+type ProvidersProps = React.PropsWithChildren<{}>;
+
+export default function Providers({ children }: ProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
