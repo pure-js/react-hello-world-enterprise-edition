aa290ab1b51d2557cc0327263c733dbadcf383dd
pure-js <3690368+pure-js@users.noreply.github.com>
2023-09-19 00:04:36 +0400

    chore: change component title name

--- components/hello-world/hello-world.stories.ts
+++ components/hello-world/hello-world.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { HelloWorld } from './hello-world';
 
 const meta = {
-  title: 'Example/Header',
+  title: 'Hello World',
   component: HelloWorld,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
