b33636024bcb866a57bdc87aa5e7193bf743e207
Årthဝur <curious.dev@icloud.com>
2023-09-19 19:19:44 +0400

    ci(deploy): change deploy to github pages config (#10)
    * ci(deploy): change github pages config
    
    * chore: commit some code
    
    * ci(lighthouse): change assertions scores
    
    * ci(deploy): change restore cache section
    
    * ci(deploy): fix config
    
    * chore: remove deprecated line since v13.30
    
    * ci: add new deploy config
    
    * ci: commit deprecated workflow

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig = {
+  output: 'export',
   distDir: 'build',
   experimental: {
     typedRoutes: true,
