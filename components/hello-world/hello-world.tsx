c4332d97c7f180cc387761d9760df51298873b06
Årthဝur <curious.dev@icloud.com>
2023-10-02 23:58:17 +0400

    fix: use dictionary

--- components/hello-world/hello-world.tsx
+++ components/hello-world/hello-world.tsx
@@ -2,17 +2,23 @@ import Image from 'next/image';
 
 import Earth from '@/public/earth.svg';
 import { Title } from './hello-world.css';
+import { getDictionary } from '../../app/dictionaries';
 
-export const HelloWorld = ({ name = 'World' }) => (
-  <>
-    <h1 className={Title}>Hello {name}!</h1>
-    <Image
-      src={Earth}
-      alt="Earth image"
-      className="dark:invert"
-      width={100}
-      height={100}
-      priority
-    />
-  </>
-);
+export const HelloWorld = async ({ name = 'World' }) => {
+  const dict = await getDictionary('en');
+  return (
+    <>
+      <h1 className={Title}>
+        {dict.hello} {name}!
+      </h1>
+      <Image
+        src={Earth}
+        alt="Earth image"
+        className="dark:invert"
+        width={100}
+        height={100}
+        priority
+      />
+    </>
+  );
+};
