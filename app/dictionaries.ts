b6217adb5abcdb6769e74696616cd3169bf9f1bd
Årthဝur <curious.dev@icloud.com>
2023-10-03 00:02:16 +0400

    fix: use dictionary (#18)
    * fix: use dictionary
    
    * chore: fix typings

---
+++ app/dictionaries.ts
@@ -0,0 +1,9 @@
+import 'server-only';
+
+const dictionaries = {
+  en: () => import('../dictionaries/en.json').then((module) => module.default),
+  es: () => import('../dictionaries/es.json').then((module) => module.default),
+};
+
+export const getDictionary = async (locale: 'es' | 'en') =>
+  dictionaries[locale]();
